2a71495c852d66bd1f1ef62e40dc910d62940d3d
Evgeniy Aleksandrov <ulthane2160@gmail.com>
2017-09-11 15:13:49 +0300

    Remove SessionId;

--- AsyncSocket.Common/Models/NetworkMessage.cs
+++ AsyncSocket.Common/Models/NetworkMessage.cs
@@ -6,7 +6,6 @@ namespace AsyncSocket.Common.Models
     [Serializable]
     public class NetworkMessage
     {
-        public SerializableGuid SessionId { get; set; }
         public MessageType Type { get; set; }
         public Byte[] Data { get; set; }
     }
